093a80288f2e3dc282d51169d15accd8bb8d97a8
taovc <tao.weijie@epitech.eu>
2023-09-27 15:48:03 +0200

    fix: axiosConfig.ts

--- my-allocine/src/framework/axiosConfig.ts
+++ my-allocine/src/framework/axiosConfig.ts
@@ -4,7 +4,6 @@ export const axiosConfig = {
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json;charset=utf-8",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDZkYjUwMjA3NDhiNTc0YTEzZjAzMTEzZWQwYzEzNiIsInN1YiI6IjY0ZjMxNjAyZTBjYTdmMDEwZGU0ZGU4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SFxxpcywreaeH7jXiRcAG37zag8dPctqAWhAYZ1qHF0",
+    Authorization: "",
   },
 };
